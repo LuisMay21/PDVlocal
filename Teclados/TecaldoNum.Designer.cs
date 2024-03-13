def811e41737c209ba4a13260d8aaf84bc1ec7d8
Luis May <123luismj@gmail.com>
2024-03-13 14:05:20 -0600

    agregado botonoes Numeros a TecladoNum

--- Teclados/TecaldoNum.Designer.cs
+++ Teclados/TecaldoNum.Designer.cs
@@ -31,6 +31,15 @@
             textBox1 = new TextBox();
             RegresarBtn = new Button();
             button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
             SuspendLayout();
             // 
             // textBox1
@@ -53,12 +62,93 @@
             // 
             // button1
             // 
-            button1.Location = new Point(293, 290);
+            button1.Location = new Point(73, 191);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(146, 130);
             button1.TabIndex = 2;
-            button1.Text = "button1";
+            button1.Text = "1";
             button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(244, 191);
+            button2.Name = "button2";
+            button2.Size = new Size(146, 130);
+            button2.TabIndex = 3;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(417, 191);
+            button3.Name = "button3";
+            button3.Size = new Size(146, 130);
+            button3.TabIndex = 4;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(73, 359);
+            button4.Name = "button4";
+            button4.Size = new Size(146, 130);
+            button4.TabIndex = 5;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(244, 359);
+            button5.Name = "button5";
+            button5.Size = new Size(146, 130);
+            button5.TabIndex = 6;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(417, 359);
+            button6.Name = "button6";
+            button6.Size = new Size(146, 130);
+            button6.TabIndex = 7;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(417, 524);
+            button7.Name = "button7";
+            button7.Size = new Size(146, 130);
+            button7.TabIndex = 10;
+            button7.Text = "9";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(244, 524);
+            button8.Name = "button8";
+            button8.Size = new Size(146, 130);
+            button8.TabIndex = 9;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(73, 524);
+            button9.Name = "button9";
+            button9.Size = new Size(146, 130);
+            button9.TabIndex = 8;
+            button9.Text = "7";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(244, 674);
+            button10.Name = "button10";
+            button10.Size = new Size(146, 130);
+            button10.TabIndex = 11;
+            button10.Text = "0";
+            button10.UseVisualStyleBackColor = true;
             // 
             // TecaldoNum
             // 
@@ -66,6 +156,15 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(803, 816);
             ControlBox = false;
+            Controls.Add(button10);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(RegresarBtn);
             Controls.Add(textBox1);
@@ -81,5 +180,14 @@
         private TextBox textBox1;
         private Button RegresarBtn;
         private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
     }
 }
